1cfdc564d903a1fb83b09b2930646030d6bedbdc
marlon-frazao <frazao.marlon@gmail.com>
2021-03-03 13:32:05 -0300

    Consulta simples com @Query

--- src/main/java/com/marlonfrazao/workshopmongo/services/PostService.java
+++ src/main/java/com/marlonfrazao/workshopmongo/services/PostService.java
@@ -31,6 +31,6 @@ public class PostService implements GenericService<Post, PostDTO, String>{
 	}
 
 	public List<Post> findByTitle(String text) {
-		return repository.findByTitleContainingIgnoreCase(text);
+		return repository.findByTitle(text);
 	}
 }

819d13d0902055ed3d377de312a7984632527a19
marlon-frazao <frazao.marlon@gmail.com>
2021-03-03 10:21:27 -0300

    Referenciando os posts do usuario

--- src/main/java/com/marlonfrazao/workshopmongo/domain/User.java
+++ src/main/java/com/marlonfrazao/workshopmongo/domain/User.java
@@ -1,8 +1,11 @@
 package com.marlonfrazao.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.marlonfrazao.workshopmongo.dto.UserDTO;
@@ -17,6 +20,9 @@ public class User implements Serializable, Convertible<UserDTO> {
 	private String id;
 	private String name;
 	private String email;
+	
+	@DBRef(lazy = true)
+	private List<Post> posts = new ArrayList<>();
 
 	public User() {
 	}
@@ -55,6 +61,14 @@ public class User implements Serializable, Convertible<UserDTO> {
 
 	public void setEmail(String email) {
 		this.email = email;
+	}
+
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
 	}
 
 	@Override
